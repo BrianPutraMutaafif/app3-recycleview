cf610fa9b3c0d3c29ad2aa23b0812e6b107f8b37
Wongsososro <briantayu57@gmail.com>
2025-08-06 08:58:11 +0700

    commit data siswa

--- app/src/main/java/com/smkth/app3_recycleview/utils/DummyData.kt
+++ app/src/main/java/com/smkth/app3_recycleview/utils/DummyData.kt
@@ -7,7 +7,17 @@ object DummyData{
         return listOf(
             Student("Andi", "123456", "XII TJKT 1"),
             Student("Okta", "123456", "XII TJKT 1"),
-            Student("Soli", "123456", "XII TJKT 1")
+            Student("Soli", "123456", "XII TJKT 1"),
+            Student("Brian", "19556", "XII TJKT 1"),
+            Student("Putra", "19556", "XII TJKT 1"),
+            Student("Muta'afif", "19556", "XII TJKT 1"),
+            Student("Damasa", "19561", "XII TJKT 1"),
+            Student("Shafa", "19561", "XII TJKT 1"),
+            Student("Ariella", "19561", "XII TJKT 1"),
+            Student("Rahayu", "19561", "XII TJKT 1"),
+            Student("Kirana", "19600", "XII TJKT 1"),
+            Student("Alifa", "19600", "XII TJKT 1"),
+            Student("Ummah", "19600", "XII TJKT 1"),
         )
     }
 }
